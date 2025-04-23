87cb529d6d4bfbc54904cbec1edd7219902c7e94
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-23 17:45:37 +0530

    Update AutoRegistration.java

--- src/test/java/com/gayu/Gayathri/AutoRegistration.java
+++ src/test/java/com/gayu/Gayathri/AutoRegistration.java
@@ -31,7 +31,7 @@ public class AutoRegistration {
 	@BeforeTest
 	public void setup() throws MalformedURLException {
 		DesiredCapabilities dc = DesiredCapabilities.chrome();
-		URL url = new URL("http://172.20.23.92:4443/wd/hub");
+		URL url = new URL("http://172.20.23.92:4444/wd/hub");
 		driver = new RemoteWebDriver(url, dc);
 	}
 
